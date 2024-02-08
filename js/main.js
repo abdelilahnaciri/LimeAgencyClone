c084a6c580cbfeabbb624335b33f0a613039014e
abdelilahnaciri <abdelilahnaciri1221@gmail.com>
2024-02-08 18:25:38 +0100

    Updated

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var navMenu = document.querySelector(".menu");
+var menuList = document.querySelectorAll(".menu_list")[0];
+var menuList2 = document.querySelectorAll(".menu_list")[1];
+document.querySelectorAll(".menu").forEach(n => n.addEventListener("click", () => {
+    if (!Boolean(menuList.style.left)) {
+        menuList.style.left = "0";
+        menuList2.style.left = "0";
+        menuList2.style.top = "300px";
+        menuList2.style.paddingTop = "0";
+    }
+    else {
+        menuList.removeAttribute('style');
+        menuList2.removeAttribute('style');
+    }
+}))
+// console.log(Boolean(menuList.style.left));
+
+// console.log(Boolean(menuList.style="display: flex;"));
